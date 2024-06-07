823b94acf774a34da5f2fef5ab70a1ee8edd91c9
omegasans279 <168962631+omegasans279@users.noreply.github.com>
2024-06-08 01:45:41 +0530

    Create OOPs.py

---
+++ Week 1/OOPs.py
@@ -0,0 +1,19 @@
+class Complex:
+    Re = None
+    Im = None
+    
+    def __init__(self, x, y):
+        self.Re = x
+        self.Im = y
+    
+    def display(self):
+        print("The complex number is =", self.Re, "+", str(self.Im) + "i")
+
+    def add(self, z):
+        return Complex(self.Re+z.Re, self.Im+z.Im)
+
+z1 = Complex(1,2)
+z1.display()
+z2 = Complex(2,3)
+z = z1.add(z2)
+z.display()
